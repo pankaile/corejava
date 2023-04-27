348e4b4edcfc2a10c315fe2ea1a57cae0eb0cf48
pankaile <435771928@qq.com>
2023-04-27 11:31:14 +0800

    submit all files

---
+++ v1ch03/FirstSample/FirstSample.java
@@ -0,0 +1,18 @@
+
+
+/**
+ * This is the first sample program in Core Java Chapter 3
+ * @version 1.01 1997-03-22
+ * @author Gary Cornell
+ */
+
+
+public class FirstSample
+{
+   public static void main(String[] args)
+   {
+      int[] a = {2,3};
+      a = new int[]{3,4};
+      System.out.println(Math.sqrt(0));
+   }
+}
